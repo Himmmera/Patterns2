8a5bfe8fa2a3751087445f9b7478a2ca4d0650e6
Himmmera <120020624+Himmmera@users.noreply.github.com>
2023-08-22 23:30:43 +0700

    AddAuthTest.java

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -1,4 +1,37 @@
 package ru.netology.test;
 
-public class AuthTest {
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
+import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
+import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $(By.tagName("button")).click();
+        $x("//h2[contains(text(),'Личный кабинет')]").shouldHave(Condition.visible);
+
+        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
+        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
+        //  пользователя registeredUser
+    }
 }
